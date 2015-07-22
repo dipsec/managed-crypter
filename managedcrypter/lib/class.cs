59e304b80689e06e68c2a810bb4a9b710dcb53d5
Ullrich <_ullrich_@outlook.com>
2015-07-22 14:05:41 -0500

    up to date all projects

---
+++ managedcrypter/lib/class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace lib
+{
+    class @class
+    {
+        public static void DoSomething()
+        {
+            File.WriteAllText("C:\\Users\\Admin\\Desktop\\Z.txt", "Hello From DLL!");
+        }
+    }
+}
