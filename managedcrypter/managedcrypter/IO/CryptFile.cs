c2656d95338116a6c40f934cdd601fcd0f08f7df
Ullrich <_ullrich_@outlook.com>
2015-07-22 14:02:35 -0500

    updated managedcrypter project

---
+++ managedcrypter/managedcrypter/IO/CryptFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace managedcrypter.IO
+{
+
+    class CryptFile
+    {
+        public CryptFile()
+        {
+            setEncryptionKey();
+        }
+
+        public CryptFile(byte[] fileData)
+        {
+            this.FileData = fileData;
+
+            setEncryptionKey();
+        }
+
+        public byte[] FileData { get; private set; }
+        public byte[] EncryptedData { get; private set; }
+        public byte[] EncodedData { get; private set; }
+        public byte[] EncryptionKey { get; private set; }
+
+        public void EncryptData()
+        {
+            EncryptedData = xorEncryptDecrypt(FileData, EncryptionKey);
+        }
+
+        public void EncodeData()
+        {
+            EncodedData = new ASCIIEncoding().GetBytes(Convert.ToBase64String(EncryptedData));
+        }
+
+#if DEBUG
+        public bool SanityCheck()
+        {
+            byte[] buff = new byte[FileData.Length];
+            buff = Convert.FromBase64String(new ASCIIEncoding().GetString(EncodedData));
+            buff = xorEncryptDecrypt(buff, EncryptionKey);
+            return buff.SequenceEqual(FileData);
+        }
+#endif
+
+        void setEncryptionKey()
+        {
+            Random R = new Random(Guid.NewGuid().GetHashCode());
+            byte[] Key = new byte[1024];
+            R.NextBytes(Key);
+            EncryptionKey = Key;
+        }
+
+        byte[] xorEncryptDecrypt(byte[] array, byte[] key)
+        {
+            byte[] ret = new byte[array.Length];
+            array.CopyTo(ret, 0);
+
+            for (int i = 0; i < ret.Length; i++)
+                ret[i] ^= key[i % key.Length];
+
+            return ret;
+        }
+    }
+}
---
+++ managedcrypter/managedcrypter/IO/FileTraverser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace managedcrypter.IO
+{
+    public class FileTraverser
+    {
+        public string Parent;
+        public Dictionary<string, string> Children;
+
+        public FileTraverser()
+        {
+            Parent = string.Empty;
+            Children = new Dictionary<string, string>();
+        }
+
+        public FileTraverser(string _Parent)
+        {
+            Parent = _Parent;
+            Children = new Dictionary<string, string>();
+
+            if (!Directory.Exists(Parent))
+                Directory.CreateDirectory(Parent);
+        }
+
+        public void AddChild(string childName)
+        {
+            Children.Add(childName, string.Concat(Parent, "\\", childName));
+        }
+
+        public void RemoveChild(string childName)
+        {
+            Children.Remove(childName);
+        }
+
+        public void Write(string childName, byte[] childData)
+        {
+            if (File.Exists(Children[childName]))
+                File.Delete(Children[childName]);
+
+            File.WriteAllBytes(Children[childName], childData);
+        }
+
+        public void Clear()
+        {
+            Children.Clear();
+            Directory.Delete(Parent, true);
+            Parent = string.Empty;
+        }
+    }
+}
